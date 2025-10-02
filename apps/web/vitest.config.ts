fadd2bc4c923a7f3f33f50dcd21fd8185f140f9d
Lucas Mafra Chagas <LMafra@users.noreply.github.com>
2025-10-02 15:34:39 -0300

    fix minitest

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: ["./src/__tests__/setup.ts"],
     coverage: {
-      provider: "istanbul",
+      provider: "v8",
       reporter: ["text", "json", "html"],
       exclude: [
         "node_modules/",
